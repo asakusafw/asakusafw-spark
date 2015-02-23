47b73ce7de8e41e71843641e28a26d6940a3b2f8
Takuya UESHIN <ueshin@nautilus-technologies.com>
2015-02-23 19:23:01 +0900

    Add class server for test.

--- backdoor/src/main/scala/org/apache/spark/backdoor/package.scala
+++ backdoor/src/main/scala/org/apache/spark/backdoor/package.scala
@@ -2,6 +2,9 @@ package org.apache.spark
 
 package object backdoor {
 
+  type HttpServer = org.apache.spark.HttpServer
+  type SecurityManager = org.apache.spark.SecurityManager
+
   implicit class SparkContextBackdoor(val sc: SparkContext) extends AnyVal {
 
     def cleanF[F <: AnyRef](f: F, checkSerializable: Boolean = true): F = {
