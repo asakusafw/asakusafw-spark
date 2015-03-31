994b0a41096c26e683917ba9723dee44ac9942d4
Takuya UESHIN <ueshin@nautilus-technologies.com>
2015-03-31 21:43:57 +0900

    Split SparkClient.execute method to prevent from "Method code too large!" error.

--- compiler/core/src/main/scala/com/asakusafw/spark/compiler/subplan/MapSubPlanCompiler.scala
+++ compiler/core/src/main/scala/com/asakusafw/spark/compiler/subplan/MapSubPlanCompiler.scala
@@ -82,22 +82,24 @@ object MapSubPlanCompiler {
       driverType: Type,
       subplan: SubPlan)(implicit context: Context): Var = {
       import context.mb._
-      val prevRddVars = subplan.getInputs.toSet[SubPlan.Input]
-        .flatMap(input => input.getOpposites.toSet[SubPlan.Output])
-        .map(_.getOperator.getSerialNumber)
-        .map(context.rddVars)
       val mapDriver = pushNew(driverType)
       mapDriver.dup().invokeInit(
         context.scVar.push(),
         context.hadoopConfVar.push(), {
           val builder = getStatic(Seq.getClass.asType, "MODULE$", Seq.getClass.asType)
             .invokeV("newBuilder", classOf[mutable.Builder[_, _]].asType)
-          prevRddVars.foreach { rddVar =>
-            builder.invokeI(
-              NameTransformer.encode("+="),
-              classOf[mutable.Builder[_, _]].asType,
-              rddVar.push().asType(classOf[AnyRef].asType))
-          }
+
+          subplan.getInputs.toSet[SubPlan.Input]
+            .flatMap(input => input.getOpposites.toSet[SubPlan.Output])
+            .map(_.getOperator.getSerialNumber)
+            .foreach { sn =>
+              builder.invokeI(NameTransformer.encode("+="), classOf[mutable.Builder[_, _]].asType,
+                context.rddsVar.push().invokeI(
+                  "apply",
+                  classOf[AnyRef].asType,
+                  ldc(sn).box().asType(classOf[AnyRef].asType)))
+            }
+
           builder.invokeI("result", classOf[AnyRef].asType).cast(classOf[Seq[_]].asType)
         })
       mapDriver.store(context.nextLocal.getAndAdd(mapDriver.size))
