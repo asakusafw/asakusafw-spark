534592d4bfe06a967b48c323a48ed2b49ec922bf
Takuya UESHIN <ueshin@nautilus-technologies.com>
2015-04-02 19:08:22 +0900

    Remove unused classes.

--- runtime/src/main/scala/com/asakusafw/spark/runtime/orderings/HashOrdering.scala
+++
@@ -1,8 +0,0 @@
-package com.asakusafw.spark.runtime.orderings
-
-class HashOrdering[T] extends Ordering[T] {
-
-  override def compare(left: T, right: T): Int = {
-    left.hashCode.compare(right.hashCode)
-  }
-}
