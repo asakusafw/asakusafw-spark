f73c1798b78312f5c905c82ae157e3da98fc9570
Takuya UESHIN <ueshin@nautilus-technologies.com>
2015-04-17 13:56:23 +0900

    Set CallSite for jobs to collect for broadcast.

--- runtime/src/main/scala/com/asakusafw/spark/runtime/SparkClient.scala
+++ runtime/src/main/scala/com/asakusafw/spark/runtime/SparkClient.scala
@@ -6,6 +6,8 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 
+import org.apache.spark.backdoor._
+import org.apache.spark.util.backdoor._
 import com.asakusafw.spark.runtime.driver.ShuffleKey
 import com.asakusafw.spark.runtime.rdd._
 
@@ -34,12 +36,19 @@ abstract class SparkClient {
     directions: Seq[Boolean],
     part: Partitioner): Broadcast[Map[ShuffleKey, Seq[V]]] = {
 
+    val name = "Prepare for Broadcast"
+    sc.clearCallSite()
+    sc.setCallSite(name)
+
     val ordering = Option(new ShuffleKey.SortOrdering(directions))
     val rdd = smcogroup(
       Seq((confluent(rdds, part, ordering).asInstanceOf[RDD[(ShuffleKey, _)]], ordering)),
       part,
       ShuffleKey.GroupingOrdering)
       .map { case (k, vs) => (k.dropOrdering, vs(0).toVector.asInstanceOf[Seq[V]]) }
+
+    sc.setCallSite(CallSite(name, rdd.toDebugString))
+
     val results =
       sc.runJob(
         rdd,
