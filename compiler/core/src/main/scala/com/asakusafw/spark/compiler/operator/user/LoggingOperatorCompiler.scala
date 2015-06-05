fce7d7582c95d3254292714c4a572fe3df6eb2aa
Takuya UESHIN <ueshin@nautilus-technologies.com>
2015-06-05 16:33:29 +0900

    Add support for @Logging operator.

---
+++ compiler/core/src/main/scala/com/asakusafw/spark/compiler/operator/user/LoggingOperatorCompiler.scala
@@ -0,0 +1,84 @@
+package com.asakusafw.spark.compiler
+package operator
+package user
+
+import scala.collection.JavaConversions._
+import scala.reflect.ClassTag
+
+import org.objectweb.asm.Type
+
+import com.asakusafw.bridge.api.Report
+import com.asakusafw.lang.compiler.model.graph.UserOperator
+import com.asakusafw.spark.compiler.spi.OperatorType
+import com.asakusafw.spark.tools.asm._
+import com.asakusafw.spark.tools.asm.MethodBuilder._
+import com.asakusafw.vocabulary.operator.Logging
+
+class LoggingOperatorCompiler extends UserOperatorCompiler {
+
+  override def support(operator: UserOperator)(implicit context: Context): Boolean = {
+    val operatorInfo = new OperatorInfo(operator)(context.jpContext)
+    import operatorInfo._
+    annotationDesc.resolveClass == classOf[Logging]
+  }
+
+  override def operatorType: OperatorType = OperatorType.MapType
+
+  override def compile(operator: UserOperator)(implicit context: Context): Type = {
+
+    val operatorInfo = new OperatorInfo(operator)(context.jpContext)
+    import operatorInfo._
+
+    assert(support(operator),
+      s"The operator type is not supported: ${annotationDesc.resolveClass.getSimpleName}")
+    assert(inputs.size == 1, // FIXME to take multiple inputs for side data?
+      s"The size of inputs should be 1: ${inputs.size}")
+    assert(outputs.size == 1,
+      s"The size of outputs should be 1: ${outputs.size}")
+
+    assert(
+      methodDesc.parameterClasses
+        .zip(inputs.map(_.dataModelClass)
+          ++: arguments.map(_.resolveClass))
+        .forall {
+          case (method, model) => method.isAssignableFrom(model)
+        },
+      s"The operator method parameter types are not compatible: (${
+        methodDesc.parameterClasses.map(_.getName).mkString("(", ",", ")")
+      }, ${
+        (inputs.map(_.dataModelClass)
+          ++: arguments.map(_.resolveClass)).map(_.getName).mkString("(", ",", ")")
+      })")
+
+    val level = Option(annotationDesc.getElements()("value"))
+      .map(_.resolve(context.jpContext.getClassLoader).asInstanceOf[Logging.Level])
+      .getOrElse(Logging.Level.getDefault)
+
+    val builder = new UserOperatorFragmentClassBuilder(
+      context.flowId,
+      inputs(Logging.ID_INPUT).dataModelType,
+      implementationClassType,
+      outputs) {
+
+      override def defAddMethod(mb: MethodBuilder, dataModelVar: Var): Unit = {
+        import mb._
+        invokeStatic(
+          classOf[Report].asType,
+          level.name.toLowerCase,
+          getOperatorField(mb)
+            .invokeV(
+              methodDesc.getName,
+              classOf[String].asType,
+              dataModelVar.push()
+                +: arguments.map { argument =>
+                  ldc(argument.value)(ClassTag(argument.resolveClass))
+                }: _*))
+        getOutputField(mb, outputs(Logging.ID_OUTPUT))
+          .invokeV("add", dataModelVar.push().asType(classOf[AnyRef].asType))
+        `return`()
+      }
+    }
+
+    context.jpContext.addClass(builder)
+  }
+}
