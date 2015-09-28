287a6c330bdd1a4b37d920caebfaf1402f2697bb
Takuya UESHIN <ueshin@nautilus-technologies.com>
2015-09-28 19:28:10 +0900

    Add tests for base flow nodes.

---
+++ core/src/test/scala/com/asakusafw/yass/RoundContextSugar.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2011-2015 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.yass
+
+import org.scalatest.Suite
+
+import scala.collection.JavaConversions._
+
+import org.apache.hadoop.conf.Configuration
+
+import com.asakusafw.bridge.stage.StageInfo
+
+trait RoundContextSugar {
+  self: SparkSugar =>
+
+  def newRoundContext(stageInfo: StageInfo): RoundContext = {
+    val conf = new Configuration(sc.hadoopConfiguration)
+    conf.set(StageInfo.KEY_NAME, stageInfo.serialize)
+
+    new RoundContext(sc, sc.broadcast(conf))
+  }
+
+  def newRoundContext(
+    userName: String = sys.props("user.name"),
+    batchId: String = "batchId",
+    flowId: String = "flowId",
+    executionId: String = "executionId",
+    batchArguments: Map[String, String] = Map("batcharg" -> "test")): RoundContext = {
+
+    val stageInfo = new StageInfo(userName, batchId, flowId, null, executionId, batchArguments)
+    newRoundContext(stageInfo)
+  }
+}
---
+++ core/src/test/scala/com/asakusafw/yass/flow/ParallelCollectionSource.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011-2015 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.yass
+package flow
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.reflect.ClassTag
+
+import org.apache.spark.rdd.RDD
+
+import com.asakusafw.spark.runtime.driver.ShuffleKey
+import com.asakusafw.spark.runtime.rdd.BranchKey
+
+class ParallelCollectionSource[T: ClassTag](
+  val branchKey: BranchKey,
+  data: Seq[T],
+  numSlices: Option[Int] = None)(
+    val label: String)
+  extends Source {
+
+  @transient
+  private var result: Future[RDD[T]] = _
+
+  override def compute(rc: RoundContext): Map[BranchKey, Future[RDD[_]]] = synchronized {
+    if (result == null) {
+      val rdd = rc.sc.parallelize(data, numSlices.getOrElse(rc.sc.defaultParallelism))
+      result = Future.successful(rdd)
+    }
+    Map(branchKey -> result)
+  }
+}
