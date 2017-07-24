aa186f4c252b3ae2fb845f13749ef393d723228f
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2017-07-24 12:25:36 +0900

    Fix for upstream (migrating `info` modules).

--- integration/src/integration-test/java/com/asakusafw/integration/spark/ToolsInfoTest.java
+++ integration/src/integration-test/java/com/asakusafw/integration/spark/ToolsInfoTest.java
@@ -16,11 +16,7 @@
 package com.asakusafw.integration.spark;
 
 import static com.asakusafw.integration.spark.Util.*;
-import static org.hamcrest.Matchers.*;
 
-import java.util.Optional;
-
-import org.junit.Assume;
 import org.junit.ClassRule;
 import org.junit.Test;
 
@@ -54,104 +50,20 @@ public class ToolsInfoTest {
     static Bundle framework;
 
     /**
-     * {@code info.sh}.
-     */
-    @Test
-    public void info() {
-        framework.withLaunch("tools/bin/info.sh");
-    }
-
-    /**
-     * {@code info.sh list batch}.
-     */
-    @Test
-    public void info_list_batch() {
-        framework.withLaunch("tools/bin/info.sh", "list", "batch", "-v");
-    }
-
-    /**
-     * {@code info.sh list batch}.
-     */
-    @Test
-    public void info_list_jobflow() {
-        checkInfo();
-        framework.withLaunch("tools/bin/info.sh", "list", "jobflow", "-v",
-                "spark.perf.average.sort");
-    }
-
-    /**
-     * {@code info.sh list batch}.
+     * {@code info.sh list plan}.
      */
     @Test
     public void info_list_plan() {
-        checkInfo();
         framework.withLaunch("tools/bin/info.sh", "list", "plan", "-v",
                 "spark.perf.average.sort");
     }
 
     /**
-     * {@code info.sh list operator}.
-     */
-    @Test
-    public void info_list_operator() {
-        checkInfo();
-        framework.withLaunch("tools/bin/info.sh", "list", "operator", "-v",
-                "spark.perf.average.sort");
-    }
-
-    /**
-     * {@code info.sh list directio-*}.
-     */
-    @Test
-    public void info_list_directio() {
-        checkInfo();
-        framework.withLaunch("tools/bin/info.sh", "list", "directio-input", "-v",
-                "spark.perf.average.sort");
-    }
-
-    /**
-     * {@code info.sh list windgate-*}.
-     */
-    @Test
-    public void info_list_windgate() {
-        checkInfo();
-        framework.withLaunch("tools/bin/info.sh", "list", "windgate-output", "-v",
-                "spark.wg.perf.average.sort");
-    }
-
-    /**
-     * {@code info.sh draw batch}.
-     */
-    @Test
-    public void info_draw_jobflow() {
-        checkInfo();
-        framework.withLaunch("tools/bin/info.sh", "draw", "jobflow", "-a",
-                "spark.perf.average.sort");
-    }
-
-    /**
-     * {@code info.sh draw batch}.
+     * {@code info.sh draw plan}.
      */
     @Test
     public void info_draw_plan() {
-        checkInfo();
-        framework.withLaunch("tools/bin/info.sh", "draw", "plan", "-a",
+        framework.withLaunch("tools/bin/info.sh", "draw", "plan", "-v",
                 "spark.perf.average.sort");
-    }
-
-    /**
-     * {@code info.sh draw operator}.
-     */
-    @Test
-    public void info_draw_operator() {
-        checkInfo();
-        framework.withLaunch("tools/bin/info.sh", "draw", "operator", "-a",
-                "spark.perf.average.sort");
-    }
-
-    private static void checkInfo() {
-        Assume.assumeThat(
-                framework.find("batchapps/spark.perf.average.sort/etc/batch-info.json"),
-                is(not(Optional.empty())));
     }
 }
