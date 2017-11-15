56259408fd59e25577f393aaaf84bdb831a4292b
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2017-11-15 15:07:10 +0900

    Bump Gradle version to 4.3.1

--- gradle/src/test/groovy/com/asakusafw/spark/gradle/plugins/AsakusaUpgradeTest.groovy
+++ gradle/src/test/groovy/com/asakusafw/spark/gradle/plugins/AsakusaUpgradeTest.groovy
@@ -51,10 +51,10 @@ class AsakusaUpgradeTest {
     }
 
     /**
-     * Test for {@code 4.3} (Asakusa on Spark {@code 0.5.0}).
+     * Test for {@code 4.3.1} (Asakusa on Spark {@code 0.5.0}).
      */
     @Test
-    void 'v4.3'() {
+    void 'v4.3.1'() {
         doUpgradeFromTestName()
     }
 
